38843494679b193e748873d567ed3043e06968ab
Werizon <bogdanskafa@gmail.com>
2021-07-20 21:53:35 +0300

    setting model data, update model student, course, add model department, instructor, officeAssignment, enrollment, update DbInitializer

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,24 @@ namespace UniversityContosoDocMicrosoft.Models
     public class Student
     {
         public int ID { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
